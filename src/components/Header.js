9bfc455d5d62c63ac2844b828efb53d79ab76bc9
Camsd <codecos.info@gmail.com>
2023-06-14 17:52:29 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../stylesheets/Header.css'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Hola mundo :D</h1>
+        </header>
+    )
+}
+
+export default Header;

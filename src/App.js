6ad04bb149d438aa6b6d9145be503315b02619b1
Camsd <codecos.info@gmail.com>
2023-06-16 10:40:56 -0500

    header style

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
-    <Header />
     <Nav />
+    <Header />
     <Main />
     <Footer />
     </>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,20 @@
 import React from "react";
 import '../stylesheets/Nav.css';
+//import images
+import logo from '../images/Logo.svg';
 
 const Nav =() =>{
     return (
         <nav>
-            soy el nav
+            <img src={logo} alt="Logo little lemon" />
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">Reservations</a></li>
+                <li><a href="#">Order Online</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
         </nav>
     )
 }

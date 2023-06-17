30536e5a9b3f26d8755ff27a9f08bc18142852d2
Camsd <codecos.info@gmail.com>
2023-06-17 17:47:06 -0500

    HTML header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,27 @@
 import React from "react";
-import '../stylesheets/Header.css'
+import "../stylesheets/Header.css";
+import food from "../images/restauranfood.jpg";
 
 const Header = () => {
-    return (
-        <header>
-            <h1>Soy el header</h1>
-        </header>
-    )
-}
+  return (
+    <header>
+      <div class="hero-content">
+        <div class="hero-text">
+          <h1>Little Lemon</h1>
+          <h4>Chicago</h4>
+          <p>
+            We are a family owned  <br />
+            Mediterranean restaurant, <br />
+            focused on traditional <br />
+            recipes served with a modern <br />
+            twist.
+          </p>
+          <button className="btn-hero">Reserve a table</button>
+        </div>
+        <img src={food} alt="food" id="food" />
+      </div>
+    </header>
+  );
+};
 
 export default Header;

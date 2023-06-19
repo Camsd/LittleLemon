41906d78ac97547b6643b801d7191eadae404ca7
Camsd <codecos.info@gmail.com>
2023-06-19 08:52:00 -0500

    main component, title and button styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,23 @@
 import React from "react";
-import '../stylesheets/Main.css'
+import "../stylesheets/Main.css";
+import salad from '../images/greek salad.jpg';
 
-const Main = () =>{
-    return (
-        <h2>soy el Main</h2>
-    );
-}
+const Main = () => {
+  return (
+    <div className="main-container">
+      <div className="main-title">
+      <h1>This week specials!</h1>
+      <button className="btn-main">Online Menu</button>
+    </div>
+      <div class="cards">
+        <img src={salad} alt="Card Image" />
+        <h2>Card Title 1</h2>
+        <p class="price">$10.99</p>
+        <p class="description">This is a description of the card.</p>
+        <button>Order Now</button>
+      </div>
+    </div>
+  );
+};
 
 export default Main;

3e78fe15a7fcc2baa15d34b2e5982a69048fa88b
Camsd <codecos.info@gmail.com>
2023-06-19 16:33:48 -0500

    cards html and css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "../stylesheets/Main.css";
 import salad from "../images/greek salad.jpg";
-import brucheta from '../images/bruchetta.svg';
-import lemonDessert from '../images/lemon dessert.jpg';
+import brucheta from "../images/bruchetta.svg";
+import lemonDessert from "../images/lemon dessert.jpg";
+import { MdDeliveryDining } from 'react-icons/md';
 
 const Main = () => {
   return (
@@ -11,27 +12,61 @@ const Main = () => {
         <h1>This week specials!</h1>
         <button className="btn-main">Online Menu</button>
       </div>
-      <div class="cards">
-        <div>
+      <div className="cards">
+        <div className="card">
           <img src={salad} alt="Greek Salad" />
-          <h2>Greek Salad</h2>
-          <p class="price">$10.99</p>
-          <p class="description">This is a description of the card.</p>
-          <button>Order Now</button>
+          <div className="cards-title">
+            <h2>Greek Salad</h2>
+            <p className="price">$ 12.99</p>
+          </div>
+          <p className="description">
+            The famous greek salad of
+            crispy lettuce, peppers, olives
+            and our Chicago style feta
+            cheese, garnished with
+            crunchy garlic and rosemary
+            croutons.
+          </p>
+          <div className="logo">
+          <p>Order a delivery</p>
+          <MdDeliveryDining />
+          </div>
         </div>
-        <div>
+        <div className="card">
           <img src={brucheta} alt="Bruchetta" />
-          <h2>Bruchetta</h2>
-          <p class="price">$10.99</p>
-          <p class="description">This is a description of the card.</p>
-          <button>Order Now</button>
+          <div className="cards-title">
+            <h2>Bruchetta</h2>
+            <p className="price">$ 5.99</p>
+          </div>
+          <p className="description">
+            Our Bruchetta is made from
+            grilled bread that has been
+            smeared with garlic and
+            seasoned with salt and olive
+            oil.
+          </p>
+          <div className="logo">
+          <p>Order a delivery</p>
+          <MdDeliveryDining />
+          </div>
         </div>
-        <div>
+        <div className="card">
           <img src={lemonDessert} alt="Lemon Dessert" />
-          <h2>Lemon Dessert</h2>
-          <p class="price">$10.99</p>
-          <p class="description">This is a description of the card.</p>
-          <button>Order Now</button>
+          <div className="cards-title">
+            <h2>Lemon Dessert</h2>
+            <p className="price">$ 5.00</p>
+          </div>
+          <p className="description">
+            This comes straight from
+            grandma's recipe book, every
+            last ingredients has been
+            sourced and is as authentic
+            as can be imagined.
+          </p>
+          <div className="logo">
+          <p>Order a delivery</p>
+          <MdDeliveryDining />
+          </div>
         </div>
       </div>
     </div>
